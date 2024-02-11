3034e12950b1d6d886ae6c486c59b5366c8bd1eb
edq <98626554+emocool@users.noreply.github.com>
2024-02-11 09:49:43 +0900

    初期コミット

--- app/chatgpt_ai/openai_helper.py
+++ app/chatgpt_ai/openai_helper.py
@@ -1,22 +1,26 @@
 from dotenv import load_dotenv
 import openai
 import os
-from llama_index import ListIndex, SimpleWebPageReader,download_loader,GPTVectorStoreIndex
+
 load_dotenv()
 
-openai.api_key = os.getenv('CHATGPT_API_KEY')
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
 def chatgpt_response(prompt):
+  from openai._client import OpenAI
+  client = OpenAI()
   
-    SimpleWebPageReader = download_loader("SimpleWebPageReader")
-    loader = SimpleWebPageReader()
-    documents = loader.load_data(urls=['https://news.yahoo.co.jp/articles/6bba0b1661ad80a578430f033eb1952809c8f4fb'])
-    index = GPTVectorStoreIndex.from_documents(documents)
-    query_engine = index.as_query_engine()
-    response = query_engine.query(prompt)
-  
-    return f"**{response}**"
+  response = client.chat.completions.create(
+    model='gpt-4',
+    messages=[
+      {"role": "system", "content": "You are a helpful agent to respond message in Japanese"},
+      {"role": "user", "content": prompt},
+    ]
     
+  )
+  print(response.choices[0].message.content)
+  return response.choices[0].message.content
+
 if __name__ == '__main__':
   res = chatgpt_response('')
   print(res)
--- app/discord_bot/discord_api.py
+++ app/discord_bot/discord_api.py
@@ -26,7 +26,7 @@ class MyClient(discord.Client):
     if command == '/ai' or command == '/bot' or command == '/chatgpt':
       bot_response = chatgpt_response(prompt=user_message)
       await message.channel.send(f"Answer: {bot_response}")
-    
-intents = discord.Intents.default()
 
+intents = discord.Intents.default()
+intents.message_content = True
 client = MyClient(intents=intents)
